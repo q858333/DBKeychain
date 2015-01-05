0c888ed9e911f862ddb62b2dbbd4a56ebe664e1d
q858333 <284825870@qq.com>
2015-01-05 15:30:12 +0800

    添加ui界面效果

--- DBKeychain/ViewController.m
+++ DBKeychain/ViewController.m
@@ -16,17 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [DBKeychainTool saveValue:@"你好啊" forKey:@"test"];
-    
+
     NSLog(@"%@",[DBKeychainTool getValueForKey:@"test"]);
-    
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)buttonClick:(id)sender
+{
+    [DBKeychainTool saveValue:_titleTextField.text forKey:@"test"];
 
+    
+    _titleLabel.text=[DBKeychainTool getValueForKey:@"test"];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
